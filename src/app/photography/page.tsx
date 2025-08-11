8106cbfbad029557e0cf14dbb693a08a2271aecf
Akreti <akretisharma06@gmail.com>
2025-08-10 22:20:44 -0400

    content update

--- src/app/photography/page.tsx
+++ src/app/photography/page.tsx
@@ -10,79 +10,79 @@ export default function Page() {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl mx-auto">
           <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo1.jpeg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo2.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Florida"}
-        date={"Feb 2024"}
+        date={"Mar 2025"}
         image={"/photo3.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Paris"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo6.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo5.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Ontario"}
-        date={"Feb 2024"}
+        date={"Aug 2024"}
         image={"/photo10.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo4.jpg"}>
         </PhotoCard>
 
         <PhotoCard
         location={"London"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo15.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Ontario"}
-        date={"Feb 2024"}
+        date={"Aug 2024"}
         image={"/photo12.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo7.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo8.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Interlaken"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo9.jpg"}>
         </PhotoCard>
         
         <PhotoCard
-        location={"Florida"}
-        date={"Feb 2024"}
+        location={"Ontario"}
+        date={"Aug 2023"}
         image={"/photo11.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Ontario"}
-        date={"Feb 2024"}
+        date={"Oct 2023"}
         image={"/photo13.jpg"}>
         </PhotoCard>
         <PhotoCard
         location={"Paris"}
-        date={"Feb 2024"}
+        date={"Aug 2023"}
         image={"/photo14.jpg"}>
         </PhotoCard>
         </div>
